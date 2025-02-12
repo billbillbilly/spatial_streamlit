0f3f74b5b90e11992af053d56425fd90161e22aa
billbillbilly <xiaohaoy111@gmail.com>
2025-02-12 10:51:16 -0500

    add feature for streaming and clip google map using polygons

--- app.py
+++ app.py
@@ -1,36 +1,34 @@
 
 import streamlit as st
+import folium
+from streamlit_folium import st_folium
 from PIL import Image
 import ollama
 
+import os
+import time
+import tempfile
+import zipfile
 import rasterio
+from rasterio.mask import mask
 import geopandas as gpd
-# from samgeo import tms_to_geotiff
+import pandas as pd
+from samgeo import tms_to_geotiff
 
 st.header('🎈 Streamlit App for Spatial Data Analysis')
 
 #--------------- Methods ---------------
 
 class processData:
-    def __init__(self, image=None, images=None):
+    def __init__(self, image=None, images=None, parcels=None):
         self.img = image
         self.imgs = images
+        self.parcels = parcels
 
-    def loadParcelsAndMap(self, parcels=None, buildings=None, map=None, bbox=None):
+    def loadParcelsAndMap(self, parcels=None, map=None, bbox=None):
         if parcels != None:
             # import shp data
-            p = gpd.read_file(parcels)
-            if buildings != None:
-                b = gpd.read_file(buildings)
-                # Ensure both layers have the same CRS
-                if p.crs != b.crs:
-                    b = b.to_crs(p.crs)
-                # select parcels with buildings
-                self.filtered_parcels = gpd.overlay(p, b, how="intersection")
-                # Save the filtered parcels
-                self.filtered_parcels.to_file("filtered_polygons.shp")
-            else:
-                self.filtered_parcels = parcels
+            self.parcels = parcels
             # import map
             if map != None:
                 m = rasterio.open(map)
@@ -46,49 +44,137 @@ class processData:
         return self.LLM_chat(system=system, prompt=prompt, img=self.img, 
                              temp=temp, top_k=top_k, top_p=top_p)
     
+    def loopImgChat(self, system=None, prompt=None, temp=None, top_k=None, top_p=None):
+        for img in self.imgs:
+            self.LLM_chat(system=system, prompt=prompt, img=img, 
+                          temp=temp, top_k=top_k, top_p=top_p)
+            
+    def loopParcelChat(self, system=None, prompt=None, temp=None, top_k=None, top_p=None):
+        progress_text = "Operation in progress. Please wait."
+        progress_bar = st.progress(0, text=progress_text)
+        dic = {
+            "lon": [],
+            "lat": [],
+            'response': [],
+        }
+        n = 0
+        for i in range(len(self.parcels)):
+            # Get the extent of one polygon from the filtered GeoDataFrame
+            polygon = self.parcels.geometry.iloc[i]
+            minx, miny, maxx, maxy = polygon.bounds
+            bbox = [minx, miny, maxx, maxy]
+            # Download data using tms_to_geotiff
+            image = "test_data/parecel.tif"
+            tms_to_geotiff(output=image, bbox=bbox, zoom=22, 
+                           source="https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", 
+                           overwrite=True)
+            # Clip the image with the polygon
+            with rasterio.open(image) as src:
+                # Reproject the polygon back to match raster CRS
+                polygon = self.parcels.to_crs(src.crs).geometry.iloc[i]
+                out_image, out_transform = mask(src, [polygon], crop=True)
+                out_meta = src.meta.copy()
+
+            out_meta.update({
+                "driver": "JPEG",
+                "height": out_image.shape[1],
+                "width": out_image.shape[2],
+                "transform": out_transform,
+                "count": 3 #Ensure RGB (3 bands)
+            })
+
+            clipped_image = 'clip.jpg'
+            with rasterio.open(clipped_image, "w", **out_meta) as dest:
+                dest.write(out_image)
+
+            res = self.LLM_chat(system=system, 
+                                prompt=prompt, 
+                                img=clipped_image, 
+                                temp=temp, 
+                                top_k=top_k, 
+                                top_p=top_p)
+            dic['lon'].append(self.parcels.centroid.x.iloc[i])
+            dic['lat'].append(self.parcels.centroid.y.iloc[i])
+            dic['response'].append(res)
+            progress_bar.progress((i+1)/len(self.parcels))
+            # # show the image
+            # with st.empty():
+            #     st.image(clipped_image, caption="clipped parcel/block", width=200)
+            #     st.write(res)
+            #     time.sleep(1)
+        return dic
+    
     def LLM_chat(self, system=None, prompt=None, img=None, temp=None, top_k=None, top_p=None):
         if prompt != None and img != None:
             res = ollama.chat(
                 model='llama3.2-vision',
-                messages=[{
-                    'role': 'user',
-                    'content': prompt,
-                    'images': [img]
-                }]
+                messages=[
+                    {
+                        'role': 'system',
+                        'content': system
+                    },
+                    {
+                        'role': 'user',
+                        'content': prompt,
+                        'images': [img]
+                    }
+                ],
+                options={
+                    "temperature":temp,
+                    "top_k":top_k,
+                    "top_p":top_p
+                }
             )
             return res['message']['content']
-                
+
+def loadSHP(file):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        # Save and extract ZIP file
+        zip_path = os.path.join(tmp_dir, "uploaded_shapefile.zip")
+        with open(zip_path, "wb") as f:
+            f.write(file.read())
+
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(tmp_dir)
+
+        # Find the .shp file, skipping __MACOSX directory and hidden files
+        shp_file = None
+        for root, dirs, files in os.walk(tmp_dir):
+            if "__MACOSX" in root:
+                continue
+            for file in files:
+                if file.endswith(".shp") and not file.startswith("._"):
+                    shp_file = os.path.join(root, file)
+                    break
+
+        if shp_file:
+            try:
+                # Read shapefile
+                gdf = gpd.read_file(shp_file)
+
+                # Ensure CRS is WGS84 for visualization
+                gdf = gdf.to_crs("EPSG:4326")
+
+                # Convert datetime columns to strings
+                for column in gdf.select_dtypes(include=["datetime", "datetime64[ns]"]).columns:
+                    gdf[column] = gdf[column].astype(str)
+                return gdf
+
+            except Exception as e:
+                st.error(f"Error reading or displaying Shapefile: {e}")
+        else:
+            st.error("No valid .shp file found in the uploaded ZIP.") 
+
+@st.cache_data
+def convert_df(data):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return data.to_csv().encode("utf-8")
+
 #--------------- User Interface & functions ----------------
 
+parcels_ = None
 image_ = None
-uploadingCol, displayCol = st.columns(2)
-# buttons for uploading files
-with uploadingCol:
-    parcel_uploader = st.file_uploader("Upload parcel data", type=["zip"])
-    if parcel_uploader:
-        st.write(f"You uploaded {parcel_uploader.name}")
-    building_uploader = st.file_uploader("Upload building footprints data", type=["zip"])
-    if parcel_uploader:
-        st.write(f"You uploaded {building_uploader.name}")
-    img_uploader = st.file_uploader("Upload image data", type=["png", "jpg", "jpeg", "tif"])
-    if img_uploader:
-        image_ = img_uploader.read()
-        st.write(f"You uploaded {img_uploader.name}")
-with displayCol:
-    displayNames = ["show image", "show parcels", "show buildings"]
-    display_ = st.radio("dispaly data", displayNames)
-    if display_ == "show image":
-        if image_:
-            st.image(image_, caption="input image")
-    elif display_ == "show parcels":
-        st.write(f"You uploaded pacels")
-    elif display_ == "show buildings":
-        st.write(f"You uploaded buildings")
-
-# interactive map
-mapView = st.toggle("get image on map")
-if mapView:
-    st.write('res')
+output = None
 
 # parameters
 para1, para2, para3 = st.columns(3)
@@ -106,10 +192,73 @@ with text1:
 with text2:
     prompt = st.text_area("Enter your prompt:")
 
+# tabs
+tab_single_img_upload, tab_parcel_upload = st.tabs(["Single Image", "Parcel/Block(shp file)"])
+
+with tab_single_img_upload:
+    # buttons for uploading files
+    img_uploader = st.file_uploader("Upload image data", type=["png", "jpg", "jpeg"])
+    if img_uploader:
+        image_ = img_uploader.read()
+        st.write(f"You uploaded {img_uploader.name}")
+        if image_:
+            st.image(image_, caption="input image")
+
+with tab_parcel_upload:
+    # buttons for uploading files
+    parcel_uploader = st.file_uploader("Upload parcel data", type=["zip"])
+    if parcel_uploader:
+        st.write(f"You uploaded {parcel_uploader.name}")
+        if parcel_uploader:
+            parcels_ = loadSHP(parcel_uploader)
+            st.dataframe(parcels_)
+
 # buttons for sending prompts/running model
-btn_send = st.button("send", key="button_send", help="click to send your prompt", type='secondary', disabled=False)
-if btn_send:
-    inputData = processData(image=image_)
-    if inputData != None:
-        res = inputData.oneImgChat(prompt=prompt, temp=tempr, top_k=top_k, top_p=top_p)
-        st.write(res)
+with tab_single_img_upload:
+    btn_send = st.button("process one image", 
+                         key="button_send", 
+                         help="click to send your prompt and image", 
+                         type='secondary', disabled=False)
+    if btn_send:
+        inputData = processData(image=image_)
+        if inputData != None:
+            res = inputData.oneImgChat(prompt=prompt, temp=tempr, top_k=top_k, top_p=top_p)
+            st.write(res)
+with tab_parcel_upload:
+    sample_on = st.toggle("randonm sample 2 rows for testing", True)
+    btn1, btn2 = st.columns(2)
+    with btn1:
+        btn_send = st.button("process vector data", 
+                            key="button_load_model", 
+                            help="click to load model for all parcels", 
+                            type='secondary', disabled=False)
+
+    if sample_on:
+        isSample = True
+    else:
+        isSample = False
+    if btn_send:
+        if isSample:
+            random_sample = parcels_.sample(n=2)
+            inputData = processData(parcels=random_sample)
+        else:
+            inputData = processData(parcels=parcels_)
+        if inputData != None:
+            res = inputData.loopParcelChat(prompt=prompt, temp=tempr, top_k=top_k, top_p=top_p)
+            # convert to dataframe
+            output = pd.DataFrame(res)
+
+    if output is not None:
+        st.dataframe(output)
+        csv = convert_df(output)
+        # download the result as csv
+        with btn2:
+            st.download_button(label="Download result as CSV", data=csv, file_name="output.csv", mime="text/csv")
+        # plot on an interactive map
+        # m = folium.Map(location=[output['lat'].mean(), output['lon'].mean()], zoom_start=12)
+        # for i, row in output.iterrows():
+        #     folium.Marker(
+        #         location=[row['lat'], row['lon']],
+        #         popup=row['response']
+        #     ).add_to(m)
+        # st_folium(m)
